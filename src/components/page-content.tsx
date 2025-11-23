6d8d8e42a2b4a8e0d37d1f9d6460af7f10ab8463
João Pedro <jpsousa471@gmail.com>
2025-11-23 11:55:36 -0300

    layout app.tsx

--- src/components/page-content.tsx
+++ src/components/page-content.tsx
@@ -2,7 +2,7 @@ import type { PageContentProps } from '@/interfaces/interfaces'
 
 export function PageContent({ children }: PageContentProps) {
   return (
-    <div className='flex w-full max-w-screen-lg flex-col gap-3 px-2 py-4'>
+    <div className='flex w-full max-w-screen-lg flex-col items-center gap-3 px-2 py-4'>
       {children}
     </div>
   )
